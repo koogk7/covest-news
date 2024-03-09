86a065e4ab09d4b41fcd67c498835cd042ef69f3
koogk7 <koogk7@gmail.com>
2024-03-09 23:35:45 +0900

    응답 직렬화

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 val ktor_version: String = "2.3.9"
@@ -7,6 +8,11 @@ val ktor_version: String = "2.3.9"
 dependencies {
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
+
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
     implementation(kotlin("stdlib-jdk8"))
 }
 
--- src/main/kotlin/com/covest/news/Application.kt
+++ src/main/kotlin/com/covest/news/Application.kt
@@ -1,14 +1,53 @@
 package com.covest.news
 
-
-
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.ExperimentalSerializationApi
 
 suspend fun main() {
-    val client = HttpClient(CIO)
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
     val response: HttpResponse = client.post("http://globalmonitor.einfomax.co.kr/apis/usa/news/getnewsall")
     println(response.bodyAsText())
+
+    val newsResponse: NewsResponse = client.post("http://globalmonitor.einfomax.co.kr/apis/usa/news/getnewsall").body()
+    println(newsResponse)
 }
+
+@Serializable
+data class NewsResponse(
+    val total: Int,
+    val data: List<NewsItem>
+)
+
+@Serializable
+data class NewsItem(
+    @SerialName("_id")
+    val id: String,
+    @SerialName("SendDateTime")
+    val sendDateTime: String,
+    @SerialName("Credit")
+    val credit: String? = null,
+    @SerialName("Category.@name")
+    val categoryName: String,
+    @SerialName("Title")
+    val title: String,
+    @SerialName("viewcount")
+    val viewCount: Int,
+    @SerialName("UpdateDateTime")
+    val updateDateTime: String,
+    @SerialName("Writer")
+    val writer: String,
+    @SerialName("Source")
+    val source: String
+)
